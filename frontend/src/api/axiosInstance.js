77cdc04783288f671f516aac49bd8b637e972275
tusharpatel777 <tussipatel826@gmail.com>
2025-07-24 11:25:49 +0530

    working fine

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000', // Your backend URL
+  baseURL: 'https://quizrush-4j74.onrender.com', // Your backend URL
 });
 
 export default axiosInstance;
